46b104df0d506ade14cc8c3da7d0be3a8ae441a4
FluffballificLathryn <arhangamebang@gmail.com>
2024-10-28 13:37:26 +0100

    Missing files retrieved, updated .gitignore and added Import.sql

---
+++ Import.sql
@@ -0,0 +1,6 @@
+-- If you like to follow the database so much, here's an empty version that creates the table for your usage.
+-- The official sql file is not for public viewing. Sorry!
+
+DROP DATABASE IF EXISTS `aerthius_world`;
+
+CREATE DATABASE `aerthius_world`;
